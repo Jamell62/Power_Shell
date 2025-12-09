f8375ce904308e9afe040dfec0576b0c76abd25f
Jamell62 <odalissevillano1712@gmail.com>
2025-12-08 23:02:32 -0500

    Update install.sh
    Install

--- install.sh
+++ install.sh
@@ -1,39 +1,30 @@
 ###################################
-# 1. Prerrequisitos
-###################################
+# Prerequisites     ----> Prerequisitos
 
-# Actualizar lista de paquetes
+# Update the list of packages   ----> Actualizar la lista de paquetes
 sudo apt-get update
 
-# Instalar dependencias necesarias
+# Install pre-requisite packages.    ---> Instalar los paquetes previos (requeridos).
 sudo apt-get install -y wget apt-transport-https software-properties-common
 
-# Obtener versión de Ubuntu
-. /etc/os-release
-# VERSION_ID queda cargado automáticamente
+# Get the version of Ubuntu  -----> Obtener la versión de Ubuntu
+source /etc/os-release
 
-# Descargar paquete del repositorio de Microsoft
-wget https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+# Download the Microsoft repository keys      ---> Descargar las claves del repositorio de Microsoft
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
 
-# Registrar la clave GPG y el repositorio
+# Register the Microsoft repository keys    ---> Registrar las claves del repositorio de Microsoft
 sudo dpkg -i packages-microsoft-prod.deb
 
-# Eliminar el archivo descargado
+# Delete the Microsoft repository keys file     ---> Eliminar el archivo de claves del repositorio de Microsoft
 rm packages-microsoft-prod.deb
 
-# Actualizar lista de paquetes (ya con el repo Microsoft agregado)
+# Update the list of packages after we added packages.microsoft.com      ---> Actualiza la lista de paquetes después de agregar packages.microsoft.com
 sudo apt-get update
 
-
 ###################################
-# 2. Instalar PowerShell
-###################################
-
+# Install PowerShell ----> Instalar Powershell
 sudo apt-get install -y powershell
 
-
-###################################
-# 3. Iniciar PowerShell
-###################################
-
+# Start PowerShell     --> Empieza Powershell
 pwsh
